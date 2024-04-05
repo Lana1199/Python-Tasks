34dde3a782c17487480807d80e71d0a91d8b6402
Lana1199 <161425027+Lana1199@users.noreply.github.com>
2024-04-05 16:24:23 +0300

    Update Task 17.7.3.py

--- Task 17.7.3.py
+++ Task 17.7.3.py
@@ -1,3 +1,4 @@
+Вариант 1
 money =int(input("Введите сумму вклада:"))
 per_cent = { 'ТКБ' : 5.6, 'СКБ':5.9 , 'ВТБ' : 4.28 , 'СБЕР' : 4.0 }
 ТКБ = int((per_cent['ТКБ']) * (money/100))
@@ -5,5 +6,16 @@ per_cent = { 'ТКБ' : 5.6, 'СКБ':5.9 , 'ВТБ' : 4.28 , 'СБЕР' : 4.0 }
 ВТБ= int((per_cent['ВТБ']) * (money/100))
 СБЕР= int((per_cent['СБЕР']) * (money/100))
 deposit =[ТКБ,СКБ,ВТБ,СБЕР]
+print("Накопленные средства за год в разных банках:" ,', '.join(map(str, deposit)))
+print("Максимальное накопление за год :", max(deposit))
+
+Вариант 2
+money =int(input("Введите сумму вклада:"))
+per_cent = { 'ТКБ' : 5.6, 'СКБ':5.9 , 'ВТБ' : 4.28 , 'СБЕР' : 4.0 }
+deposit =[]
+deposit.append(int((per_cent['ТКБ']) * (money/100)))
+deposit.append(int((per_cent['СКБ']) * (money/100)))
+deposit.append(int((per_cent['ВТБ']) * (money/100)))
+deposit.append(int((per_cent['СБЕР']) * (money/100)))
 print("Накопленные средства за год в разных банках:" ,', '.join(map(str, deposit)))
 print("Максимальное накопление за год :", max(deposit))
